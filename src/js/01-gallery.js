7f8936f64a2efe29d116254a52af6dbaccb9badd
damax87 <migunoff87@gmail.com>
2022-12-30 22:07:09 +0200

    4 commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,29 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+
+const cardsMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        `;
+    }).join(``);
+    };
+
+galleryContainer.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+new SimpleLightbox('.gallery a', {});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function(currentTime) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds));
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function(error) {
+    console.error(error);
+    });
